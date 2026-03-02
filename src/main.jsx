7695d0137806565387c3f9d42e24f563c0ff3d52
areful75 <mrarefulomi@gmail.com>
2026-03-02 16:07:01 +0600

    added routing and navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
 import './index.css'
 import App from './App.jsx'
+import Root from './Layout/Root.jsx';
+import Home from './components/Home.jsx';
+import Login from './components/Login.jsx';
+import Register from './components/Register.jsx';
+const router = createBrowserRouter([
+  {
+    path: "/",
+   Component:Root,
+   children:[
+    {index:true,Component:Home},
+    {path:'/login',Component:Login},
+    {path:'/register',Component:Register},
 
+
+   ]
+  },
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
